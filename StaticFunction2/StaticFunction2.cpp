148cb684cb7cc5559bd6fcc7843c286629ac11cc
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-05-15 15:58:21 +0700

    d

--- StaticFunction2/StaticFunction2.cpp
+++ StaticFunction2/StaticFunction2.cpp
@@ -16,3 +16,32 @@ public:
 
 	mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+	id = ++nim;		// ++nim = ditambah dulu baru digunakan, misal nim = 0, maka nim selanjutnya 1 + 0
+}
+
+void mahasiswa::printAll() {
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+int main() {
+	mahasiswa mhs1("Ratih");
+	mahasiswa mhs2("Nandu");
+	mahasiswa::setNim(9);		//mengakses nim melalui static member function "setNim"
+	mahasiswa mhs3("Rezza");
+	mahasiswa mhs4("Rasya");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	cout << "Akses dari luar object = " << mahasiswa::getNim() << endl;	//mengakses nim
+
+	return 0;
+}
