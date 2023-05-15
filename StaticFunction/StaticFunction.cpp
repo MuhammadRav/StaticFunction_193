6b3b0d9ec3f7ba2cfaac43e4256c1c91a1f79b77
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-05-15 15:38:52 +0700

    b

--- StaticFunction/StaticFunction.cpp
+++ StaticFunction/StaticFunction.cpp
@@ -15,9 +15,24 @@ public:
 int mahasiswa::nim = 0;
 
 void mahasiswa::setID() {
-	id = ++nim;
+	id = ++nim;		// ++nim = ditambah dulu baru digunakan, misal nim = 0, maka nim selanjutnya 1 + 0
 }
 
 void mahasiswa::printAll() {
 	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+int main() {
+	mahasiswa mhs1("Ratih");
+	mahasiswa mhs2("Nandu");
+	mahasiswa mhs3("Rezza");
+	mahasiswa mhs4("Rasya");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+	return 0;
 }
